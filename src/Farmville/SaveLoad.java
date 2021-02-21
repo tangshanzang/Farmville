5b71c4ad47378b0c646613bad7a51ac9abfeada0
TangShanZang <odielee@hotmail.com>
2021-02-21 15:35:43 +0100

    Changed the save/load function from selecting save slot to letting user decide name for save/load file

--- src/Farmville/SaveLoad.java
+++ src/Farmville/SaveLoad.java
@@ -1,10 +1,11 @@
 package Farmville;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class SaveLoad implements Serializable {
     String saveSlot, savePathInString;
@@ -18,27 +19,21 @@ public class SaveLoad implements Serializable {
     public void saveGame() {
         boolean pass = false;
         while (!pass) {
-            int number = Dialog.promptInt("Please choose a save:\n1.Save1\n2.Save2\n3.Save3\n4.Exit", "Choose a save on the menu", "Numbers only!", 1, 4);
-            switch (number) {
-                case 1 -> saveSlot = "Save1";
-                case 2 -> saveSlot = "Save2";
-                case 3 -> saveSlot = "Save3";
-                case 4 -> pass = true;
-            }
-            savePathInString = saveSlot + ".ser";
+            saveSlot = Dialog.prompt("Please enter a name for your save");
+            String root = "saves/";
+            savePathInString = root + saveSlot.toLowerCase() + ".ser";
             Path savePath = Paths.get(savePathInString);
             boolean exist = Files.exists(savePath);
             if (!exist) {
-
                 Serializer.serialize(savePathInString, myGame);
                 System.out.println("Game saved");
                 pass = true;
-            }
-            else{
-                number = Dialog.promptInt("Save is occupied, do you want to overwrite the save?\n1.Yes\n2.No"
-                        , "Choose a save on the menu", "Numbers only!", 1, 2);
+            } else {
+                int number = Dialog.promptInt("Save is occupied, do you want to overwrite the save?\n1.Yes\n2.No"
+                        , "Choose an option on the menu", "Numbers only!", 1, 2);
                 switch (number) {
-                    case 1 -> { Serializer.serialize(savePathInString, myGame);
+                    case 1 -> {
+                        Serializer.serialize(savePathInString, myGame);
                         System.out.println("Game saved");
                         pass = true;
                     }
@@ -51,48 +46,45 @@ public class SaveLoad implements Serializable {
     }
 
     public void loadGame() {
-        System.out.println("-".repeat(50));
         boolean pass = false;
+        File folderPath = new File("saves/");
+        FilenameFilter serFilter = new FilenameFilter() { // Create a filter to find every file that ends with .ser
+            @Override
+            public boolean accept(File file, String name) {
+                return name.endsWith(".ser");
+            }
+        };
+        String[] serFiles = folderPath.list(serFilter); // find .ser in saves folder
         while (!pass) {
-            System.out.println("Select the save file you would like to load:");
-            System.out.println("1.Save1 ");
-            System.out.println("2.Save2 ");
-            System.out.println("3.Save3 ");
-            System.out.println("4.Exit ");
-            Scanner myScanner = new Scanner(System.in);
-            String input = myScanner.nextLine();
-            System.out.println("-".repeat(50));
-
-            switch (input) {
-                case "1" -> {
-                    if (!Files.exists(Paths.get("Save1.ser"))) {
-                        System.out.println("Save is empty");
-                    } else {
-                        myGame = (Game) Serializer.deserialize("Save1.ser");
-                        myGame.mainGamePreRound();
-                        pass = true;
+            if (serFiles != null && serFiles.length > 0) {
+                System.out.println("Your saves : ");
+                for (String name : serFiles)
+                    System.out.println(name.replaceAll(".ser", "")); // remove .ser to look user friendly
+                System.out.println("-".repeat(50));
+                String a = Dialog.prompt("Enter name of the save you would want to load.");
+                String loadString = "saves/" + a.toLowerCase() + ".ser";
+                File tryPath = new File(loadString);
+                if (tryPath.exists()) {
+                    pass = true;
+                    System.out.println("Game loaded");
+                    myGame = (Game) Serializer.deserialize(loadString);
+                    myGame.mainGamePreRound();
+                } else {
+                    System.out.println("Save does not exist, please make sure you entered the correct name for the save");
+                    System.out.println("-".repeat(50));
+                    int choice = Dialog.promptInt("1. Try again\n2. Exit","Select an option on the menu!","Numbers only!",1,2);
+                    switch (choice){
+                        case 1 -> {}
+                        case 2 -> pass = true;
                     }
                 }
-                case "2" -> {
-                    if (!Files.exists(Paths.get("Save2.ser"))) {
-                        System.out.println("Save is empty");
-                    } else {
-                        myGame = (Game) Serializer.deserialize("Save2.ser");
-                        myGame.mainGamePreRound();
-                        pass = true;
-                    }
-                }
-                case "3" -> {
-                    if (!Files.exists(Paths.get("Save3.ser"))) {
-                        System.out.println("Save is empty");
-                    } else {
-                        myGame = (Game) Serializer.deserialize("Save3.ser");
-                        myGame.mainGamePreRound();
-                        pass = true;
-                    }
-                }
-                case "4" -> pass = true;
+            }
+            else{
+                System.out.println("You don't have any save!");
+                pass = true;
             }
         }
     }
 }
+
+
